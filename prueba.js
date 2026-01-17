72b8576f948703c5715d5dd94c812355d45a3ea6
brandonstevenz2002 <brandonstevenvm@gmail.com>
2026-01-16 22:42:21 -0500

    aprendiendo sentencias

---
+++ prueba.js
@@ -0,0 +1,33 @@
+var $a = 0
+
+console.log($a)
+
+var _a = 2
+
+console.log(_a)
+
+var -a = 8
+
+var 1a = 2
+
+var arbol = "Arbol"
+
+function arbol () {
+
+}
+
+var mi_archivo_prueba.js 
+
+const UNA_CONSTANTE = "Soy una constante"
+PI = 3.14
+
+class SerHumano {
+    constructor (nombre, genero) {
+        this.nombre = nombre
+        this.genero = genero 
+    }
+
+    miNombreEs () {
+        return 'Mi nombre es ' + this.nombre
+    }
+}
